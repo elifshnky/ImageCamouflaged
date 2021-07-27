e2ce0cc91b97fd2a543cfba67603029b872f20d8
elif <elifshnky@gmail.com>
2021-07-28 01:42:26 +0300

    Camouflaged
    Görüntü işleme-Seçilen görselde nesneyi kamufle etmek amaçlanmıştır.

---
+++ ImageCamouflaged/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ImageCamouflaged
+{
+    partial class CamouflageWindow
+    {
+
+        private System.ComponentModel.IContainer componentsImage = null;
+
+        protected override void Dispose(bool dispos)
+        {
+            if (dispos && (componentsImage != null))
+            {
+                componentsImage.Dispose();
+            }
+            base.Dispose(dispos);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.ImageAfter = new System.Windows.Forms.PictureBox();
+            this.ImageBefore = new System.Windows.Forms.PictureBox();
+            this.btnCamouflaged = new System.Windows.Forms.Button();
+            this.beforelabel = new System.Windows.Forms.Label();
+            this.afterlabel = new System.Windows.Forms.Label();
+            this.messagelabel = new System.Windows.Forms.Label();
+            this.ImageGrey = new System.Windows.Forms.PictureBox();
+            this.greylabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageAfter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBefore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageGrey)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageAfter
+            // 
+            this.ImageAfter.BackColor = System.Drawing.Color.Orange;
+            this.ImageAfter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ImageAfter.Location = new System.Drawing.Point(462, 86);
+            this.ImageAfter.Margin = new System.Windows.Forms.Padding(2);
+            this.ImageAfter.Name = "ImageAfter";
+            this.ImageAfter.Size = new System.Drawing.Size(436, 421);
+            this.ImageAfter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageAfter.TabIndex = 7;
+            this.ImageAfter.TabStop = false;
+            // 
+            // ImageBefore
+            // 
+            this.ImageBefore.BackColor = System.Drawing.Color.SlateBlue;
+            this.ImageBefore.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ImageBefore.Location = new System.Drawing.Point(22, 86);
+            this.ImageBefore.Margin = new System.Windows.Forms.Padding(2);
+            this.ImageBefore.Name = "ImageBefore";
+            this.ImageBefore.Size = new System.Drawing.Size(436, 421);
+            this.ImageBefore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageBefore.TabIndex = 4;
+            this.ImageBefore.TabStop = false;
+            // 
+            // ImageGrey
+            // 
+            this.ImageGrey.BackColor = System.Drawing.Color.Silver;
+            this.ImageGrey.Location = new System.Drawing.Point(903, 86);
+            this.ImageGrey.Name = "p2";
+            this.ImageGrey.Size = new System.Drawing.Size(436, 421);
+            this.ImageGrey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageGrey.TabIndex = 11;
+            this.ImageGrey.TabStop = false;
+            // 
+            // btnCamouflaged
+            // 
+            this.btnCamouflaged.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnCamouflaged.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnCamouflaged.FlatAppearance.BorderSize = 4;
+            this.btnCamouflaged.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki;
+            this.btnCamouflaged.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.btnCamouflaged.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCamouflaged.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCamouflaged.ForeColor = System.Drawing.Color.Indigo;
+            this.btnCamouflaged.Location = new System.Drawing.Point(499, 522);
+            this.btnCamouflaged.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCamouflaged.Name = "btnCamouflaged";
+            this.btnCamouflaged.Size = new System.Drawing.Size(357, 68);
+            this.btnCamouflaged.TabIndex = 5;
+            this.btnCamouflaged.Text = "Camouflage the picture";
+            this.btnCamouflaged.UseVisualStyleBackColor = false;
+            this.btnCamouflaged.Click += new System.EventHandler(this.ButtonClickfile);
+            // 
+            // beforelabel
+            // 
+            this.beforelabel.AutoSize = true;
+            this.beforelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.beforelabel.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.beforelabel.Location = new System.Drawing.Point(171, 48);
+            this.beforelabel.Name = "beforelabel";
+            this.beforelabel.Size = new System.Drawing.Size(112, 36);
+            this.beforelabel.TabIndex = 8;
+            this.beforelabel.Text = "-Before";
+            // 
+            // afterlabel
+            // 
+            this.afterlabel.AutoSize = true;
+            this.afterlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.afterlabel.ForeColor = System.Drawing.Color.Orange;
+            this.afterlabel.Location = new System.Drawing.Point(617, 48);
+            this.afterlabel.Name = "afterlabel";
+            this.afterlabel.Size = new System.Drawing.Size(95, 36);
+            this.afterlabel.TabIndex = 9;
+            this.afterlabel.Text = "+After";
+            // 
+            // greylabel
+            // 
+            this.greylabel.AutoSize = true;
+            this.greylabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.greylabel.ForeColor = System.Drawing.Color.Gray;
+            this.greylabel.Location = new System.Drawing.Point(1078, 47);
+            this.greylabel.Name = "label1";
+            this.greylabel.Size = new System.Drawing.Size(96, 36);
+            this.greylabel.TabIndex = 12;
+            this.greylabel.Text = "#Grey";
+            // 
+            // messagelabel
+            // 
+            this.messagelabel.AutoSize = true;
+            this.messagelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.messagelabel.ForeColor = System.Drawing.Color.SteelBlue;
+            this.messagelabel.Location = new System.Drawing.Point(514, 601);
+            this.messagelabel.Name = "messagelabel";
+            this.messagelabel.Size = new System.Drawing.Size(0, 20);
+            this.messagelabel.TabIndex = 10;
+            // 
+            // CamouflageWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1361, 645);
+            this.Controls.Add(this.greylabel);
+            this.Controls.Add(this.ImageGrey);
+            this.Controls.Add(this.messagelabel);
+            this.Controls.Add(this.afterlabel);
+            this.Controls.Add(this.beforelabel);
+            this.Controls.Add(this.ImageAfter);
+            this.Controls.Add(this.btnCamouflaged);
+            this.Controls.Add(this.ImageBefore);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "CamouflageWindow";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CamouflageWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageAfter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBefore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageGrey)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageAfter;
+        private System.Windows.Forms.PictureBox ImageBefore;
+        private System.Windows.Forms.PictureBox ImageGrey;
+        private System.Windows.Forms.Button btnCamouflaged;
+        private System.Windows.Forms.Label beforelabel;
+        private System.Windows.Forms.Label afterlabel;
+        private System.Windows.Forms.Label greylabel;
+        private System.Windows.Forms.Label messagelabel;
+        
+    }
+}
+
---
+++ ImageCamouflaged/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCamouflaged
+{
+    public partial class CamouflageWindow : Form
+    {
+
+
+        public CamouflageWindow()
+        {
+            InitializeComponent();
+            this.messagelabel.Text = "Kamufle etmek istediğiniz göseli seçiniz.";
+            this.messagelabel.ForeColor = System.Drawing.Color.SteelBlue;
+        }
+
+
+        private void ButtonClickfile(object sender, EventArgs a)
+        {
+            OpenFileDialog File = new OpenFileDialog();
+            File.Filter = "Image Files(*.JPEG;*.JPG*;.PNG;*.SVG)|*.JPEG;*.JPG;*.PNG;*.SVG";
+
+            var dialogResult = File.ShowDialog();
+
+            if (dialogResult == DialogResult.OK || dialogResult == DialogResult.Yes)
+            {
+                Bitmap image = new Bitmap(File.FileName);
+
+                ImageBefore.Image = image;
+
+                Bitmap CamouflagedImage = CamouflageImage(in image);
+
+                Bitmap GreyI = Grey(in image);//kamufle edilmiş halini gri seviyeye getirmek için imCamouflagedImage
+
+                ImageAfter.Image = CamouflagedImage;
+                ImageGrey.Image = GreyI;
+
+                this.messagelabel.Text = "    >>>>  Görüntü Kamufle Edildi  <<<<";
+                this.messagelabel.ForeColor = System.Drawing.Color.Orange;
+
+            }
+            else
+            {
+                this.messagelabel.Text = "     -->  Görsel seçimi yapmadınız  <--";
+                this.messagelabel.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            }
+        }
+
+
+        private void ColorMix(Bitmap image, List<BitmapRowDetail> borderpointList, List<Color> colors)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < borderpointList.Count; i++)
+            {
+                for (int j = borderpointList[i].StartColum; j < borderpointList[i].EndColum; j++)
+                {
+                    var randomdeger = random.Next(0, colors.Count());
+
+                    image.SetPixel(i, j, colors[randomdeger]);
+                }
+            }
+        }
+
+
+        private Bitmap CamouflageImage(in Bitmap image)
+        {
+            Bitmap CImage = image.Clone() as Bitmap;
+
+            List<Color> differentColors = GetDifferentColorsListBitmap(CImage, 10);
+            List<BitmapRowDetail> borderCoordinateL = GetBorderPoints(CImage);
+
+            ColorMix(CImage, borderCoordinateL, differentColors);
+
+            return CImage;
+        }
+
+
+        private Bitmap Grey(in Bitmap image)
+        {
+
+            Bitmap grey = image.Clone() as Bitmap;
+            Color colordata;
+            byte greydata, red, green, blue;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    colordata = image.GetPixel(i, j);
+                    red = colordata.R;
+                    green = colordata.G;
+                    blue = colordata.B;
+
+                    greydata = Convert.ToByte((red + green + blue) / 3);
+                    grey.SetPixel(i, j, Color.FromArgb(greydata, greydata, greydata));
+                }
+            }
+            return grey;
+
+        }
+
+
+        private List<BitmapRowDetail> GetBorderPoints(Bitmap image)
+        {
+            List<BitmapRowDetail> borderpointList = new List<BitmapRowDetail>();
+
+            for (int i = 1; i < image.Width - 1; i++)
+            {
+                BitmapRowDetail RowDetail = new BitmapRowDetail();
+
+                RowDetail.Row = i;
+
+                for (int j = 1; j < image.Height - 1; j++)
+                {
+                    Color ColorLeft = image.GetPixel(i - 1, j);
+                    Color ColorRight = image.GetPixel(i + 1, j);
+                    Color ColorUp = image.GetPixel(i, j - 1);
+                    Color ColorDown = image.GetPixel(i, j + 1);
+                    int dx = ColorRight.R - ColorLeft.R;
+                    int dy = ColorDown.R - ColorUp.R;
+                    double Precision = Math.Sqrt(dx * dx / 4 + dy * dy / 4);
+
+                    if (Precision > 9)//0b1010
+                    {
+                        if (RowDetail.StartColum == 0)
+                        {
+                            RowDetail.StartColum = j;
+                        }
+                        else
+                        {
+                            RowDetail.EndColum = j;
+                        }
+                    }
+                }
+
+                borderpointList.Add(RowDetail);
+            }
+
+            return borderpointList;
+
+        }
+
+
+        private List<Color> GetDifferentColorsListBitmap(Bitmap image, int count)
+        {
+            List<Color> colorsIm = new List<Color>();
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var colorIm = image.GetPixel(x, y);
+                    colorsIm.Add(colorIm);
+                }
+            }
+
+            return colorsIm
+                .GroupBy(k => k)
+                .Select(groupIm => new
+                {
+                    Key = groupIm.Key,
+                    Count = groupIm.Count()
+                })
+                .OrderByDescending(k => k.Count)
+                .Take(count)
+                .Select(k => k.Key)
+                .ToList();
+        }
+
+    }
+
+
+    class BitmapRowDetail
+    {
+        public int Row { get; set; }
+        public int StartColum { get; set; }
+        public int EndColum { get; set; }
+    }
+
+
+}
